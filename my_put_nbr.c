8e22fe452eaed9d5239083ddf75bf87c5de8946e
ange <ange@localhost.localdomain>
2020-10-11 14:41:04 +0200

    Updated

--- my_put_nbr.c
+++ my_put_nbr.c
@@ -7,24 +7,6 @@
 
 int my_put_nbr(int nb)
 {
-<<<<<<< HEAD
-    char c;
-    char str[];
-
-    for (int i = 0; nb > 0; i++) {
-        c = nb % 10 + '0';
-        str[i] = c;
-        nb /= 10;
-    }
-    my_putstr(str);
-    return (0);
-}
-
-int main()
-{
-    int nb = 123;
-    my_put_nbr(nb);
-=======
     int nbr = nb;
     int i;
 
@@ -33,5 +15,4 @@ int main()
     my_putchar('0' + nbr);
     if (nbr != nb)
         my_put_nbr(nb - nbr * 10 * i);
->>>>>>> db8d825ef3794a071a172db5ef9ec93bca14a75c
 }
