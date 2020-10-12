e42b5a0a080be91efa3c44c28feda995dc6aba36
ange <ange@localhost.localdomain>
2020-10-12 18:26:05 +0200

    Updated

--- my/my_getnbr.c
+++ my/my_getnbr.c
@@ -7,5 +7,13 @@
 
 int my_getnbr(char const *str)
 {
+    int nb;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        for (; '0' <= str[i] && str[i] <= '9'; i++)
+            nb = nb * 10 + str[i] - '0';
+        if (nb >= 0)
+            return(nb);
+    }
     return (0);
 }
