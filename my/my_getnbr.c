2f9bd6829f91804334bb629ec6d066b848b42b27
ange <ange@localhost.localdomain>
2020-10-13 18:02:42 +0200

    Updated

--- my/my_getnbr.c
+++ my/my_getnbr.c
@@ -7,7 +7,7 @@
 
 #include <stdio.h>
 
-int is_num(char c)
+int my_is_num(char c)
 {
     if ('0' <= c && c <= '9') {
         return (1);
@@ -17,16 +17,16 @@ int is_num(char c)
 
 int my_getnbr(char const *str)
 {
-    int nb;
+    int nb = 0;
     int neg = 1;
     int test = 0;
 
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] == '-')
             neg *= -1;
-        else if (!is_num(str[i]) && str[i] != '+')
+        else if (!my_is_num(str[i]) && str[i] != '+')
             neg = 1;
-        for (; is_num(str[i]); i++) {
+        for (; my_is_num(str[i]); i++) {
             nb = nb * 10 + str[i] - '0';
             test = 1;
         }
--- my/my_put_nbr.c
+++ my/my_put_nbr.c
@@ -9,10 +9,15 @@ void my_putchar(char);
 
 int my_put_nbr(int nb)
 {
-    int nbr = nb;
+    int nbr;
     int i;
 
-    for (i = 0; nbr > 10; i++)
+    if (nb < 0) {
+        my_putchar('-');
+        nb *= -1;
+    }
+    nbr = nb;
+    for (i = 0; nbr >= 10; i++)
         nbr /= 10;
     my_putchar('0' + nbr);
     if (nbr != nb)
