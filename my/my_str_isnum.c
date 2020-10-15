f29052552346157ea923e9c6f99a6992db406dd6
ange <ange@localhost.localdomain>
2020-10-15 11:01:33 +0200

    Updated

--- my/my_str_isnum.c
+++ my/my_str_isnum.c
@@ -5,13 +5,9 @@
 ** my_str_isnum
 */
 
-int my_strlen(char const *);
-
 int my_str_isnum(char const *str)
 {
-    int len = my_strlen(str);
-
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; str[i] != '\0'; i++) {
         if (!('0' <= str[i] && str[i] <= '9')) {
             return (0);
         }
