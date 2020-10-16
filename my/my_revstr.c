75e9b284b752a1a38971e8b32cb8e318319f5e42
ange <ange@localhost.localdomain>
2020-10-16 20:13:07 +0200

    Updated

--- my/my_revstr.c
+++ my/my_revstr.c
@@ -5,6 +5,8 @@
 ** my_revstr
 */
 
+#include <stdio.h>
+
 int my_strlen(char const *);
 
 char *my_revstr(char *str)
@@ -12,10 +14,16 @@ char *my_revstr(char *str)
     char c;
     int len = my_strlen(str) - 1;
 
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len / 2; i++) {
         c = str[i];
         str[i] = str[len - i];
         str[len - i] = c;
     }
     return (str);
+}
+
+int main (void)
+{
+    char str[] = "0345";
+    printf("%s\n", my_revstr(str));
 }
