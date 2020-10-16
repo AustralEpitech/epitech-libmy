840b82771614f02d8ff404605d389746305b3608
ange <ange@localhost.localdomain>
2020-10-16 20:20:01 +0200

    Updated

--- my/my_revstr.c
+++ my/my_revstr.c
@@ -14,10 +14,16 @@ char *my_revstr(char *str)
     char c;
     int len = my_strlen(str) - 1;
 
-    for (int i = 0; i < len / 2; i++) {
+    for (int i = 0; i < (len + 1) / 2; i++) {
         c = str[i];
         str[i] = str[len - i];
         str[len - i] = c;
     }
     return (str);
+}
+
+int main (void)
+{
+    char str[] = "abcdef";
+    printf("%s\n", my_revstr(str));
 }
