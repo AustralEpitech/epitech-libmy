29b9448be3996ea931d1c4c01283ae2faff0cd3c
AngeD <ange.duhayon@epitech.eu>
2021-01-01 15:33:42 +0100

    fix: my_strcat.c

--- newrepo/lib/my/my_str.c
+++ newrepo/lib/my/my_str.c
@@ -18,9 +18,9 @@ int my_strlen(char const *str)
 char *my_strcat(char *dest, char const *src)
 {
     int len = my_strlen(dest);
-    int i;
+    int i = 0;
 
-    for (i = 0; i < len; i++)
+    for (; src[i] != '\0'; i++)
         dest[len + i] = src[i];
     dest[len + i] = '\0';
     return (dest);
