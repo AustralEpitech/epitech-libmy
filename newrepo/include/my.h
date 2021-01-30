1d1781703523f07e38583f56026714b4b75d43e1
AngeD <ange.duhayon@epitech.eu>
2021-01-30 14:40:14 +0100

    feat: str_to_word, added skip

--- newrepo/include/my.h
+++ newrepo/include/my.h
@@ -31,6 +31,6 @@ char *my_strlowcase(char *str);
 char *my_strupcase(char *str);
 char *my_strstr(char *str, char const *to_find);
 void my_swap(int *a, int *b);
-char **my_str_to_word_array(char *str, char sep);
+char **my_str_to_word_array(char *str, char sep, char skip);
 
 #endif /* MY_H_ */
--- newrepo/lib/my/my_str_to_word_array.c
+++ newrepo/lib/my/my_str_to_word_array.c
@@ -7,15 +7,17 @@
 
 #include <stdlib.h>
 
-static int my_strlen_sep(char *str, char sep)
+static int my_strlen_sep(char *str, char sep, char skip)
 {
-    int i = 0;
+    int count = 0;
 
-    for (; str[i] && str[i] != sep; i++);
-    return (i);
+    for (int i = 0; str[i] && str[i] != sep; i++)
+        if (str[i] != skip)
+            count++;
+    return (count);
 }
 
-char **my_str_to_word_array(char *str, char sep)
+char **my_str_to_word_array(char *str, char sep, char skip)
 {
     char **sstr = NULL;
     int count = 1;
@@ -25,12 +27,15 @@ char **my_str_to_word_array(char *str, char sep)
         if (str[index] == sep)
             count++;
     sstr = malloc(sizeof(char *) * (count + 1));
-    for (int j = 0, index = 0; i < count; i++, j = 0, index++) {
-        sstr[i] = malloc(my_strlen_sep(str + index, sep) + 1);
-        for (; str[index] && str[index] != sep; index++, j++)
-            sstr[i][j] = str[index];
-        sstr[i][j] = 0;
+    sstr[0] = malloc(my_strlen_sep(str, sep, skip));
+    for (int j = 0, index = 0; str[index]; index++) {
+        if (str[index] == sep) {
+            sstr[i++][j] = 0;
+            j = 0;
+            sstr[i] = malloc(my_strlen_sep(str + index, sep, skip) + 1);
+        } else if (str[index] != skip)
+            sstr[i][j++] = str[index];
     }
-    sstr[i] = NULL;
+    sstr[++i] = NULL;
     return (sstr);
 }
