7a032e2973a207c83307215d088bb3a69115e1db
AngeD <ange.duhayon@epitech.eu>
2021-01-31 10:06:46 +0100

    my_str_to_word_array: mem

--- newrepo/lib/my/my_str_to_word_array.c
+++ newrepo/lib/my/my_str_to_word_array.c
@@ -7,14 +7,30 @@
 
 #include <stdlib.h>
 
+static int is_sep(char c, char next, char sep, char skip)
+{
+    if (c == sep && next)
+        if (next != skip && next != sep)
+            return (1);
+    return (0);
+}
+
 static int my_strlen_sep(char *str, char sep, char skip)
 {
     int count = 0;
+    int i = 0;
 
-    for (int i = 0; str[i] && str[i] != sep; i++)
+    for (; str[i] && str[i] != sep; i++)
         if (str[i] != skip)
             count++;
     return (count);
+}
+
+static char **end_str(char **sstr, int i, int j)
+{
+    sstr[i][j] = 0;
+    sstr[++i] = NULL;
+    return (sstr);
 }
 
 char **my_str_to_word_array(char *str, char sep, char skip)
@@ -22,20 +38,21 @@ char **my_str_to_word_array(char *str, char sep, char skip)
     char **sstr = NULL;
     int count = 1;
     int i = 0;
+    int j = 0;
 
     for (int index = 0; str[index]; index++)
-        if (str[index] == sep)
+        if (is_sep(str[index], str[index + 1], sep, skip))
             count++;
     sstr = malloc(sizeof(char *) * (count + 1));
-    sstr[0] = malloc(my_strlen_sep(str, sep, skip));
-    for (int j = 0, index = 0; str[index]; index++) {
-        if (str[index] == sep) {
+    sstr[0] = malloc(my_strlen_sep(str, sep, skip) + 1);
+    for (int index = 0; str[index]; index++) {
+        if (str[index] != skip && str[index] != sep)
+            sstr[i][j++] = str[index];
+        else if (is_sep(str[index], str[index + 1], sep, skip)) {
             sstr[i++][j] = 0;
             j = 0;
-            sstr[i] = malloc(my_strlen_sep(str + index, sep, skip) + 1);
-        } else if (str[index] != skip)
-            sstr[i][j++] = str[index];
+            sstr[i] = malloc(my_strlen_sep(str + index + 1, sep, skip) + 1);
+        }
     }
-    sstr[++i] = NULL;
-    return (sstr);
+    return (end_str(sstr, i, j));
 }
