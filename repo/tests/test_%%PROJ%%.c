ea88e24fc0ccbc04818a6c67d9d95a28ece960e6
AngeD <ange.duhayon@epitech.eu>
2021-04-16 15:05:32 +0200

    feat: tests, debug Makefile flag. fix: new_proj if N

---
+++ repo/tests/test_%%PROJ%%.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2021
+** %%PROJ
+** File description:
+** test_%%PROJ%%
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+int %%PROJ%%(void);
+
+void redirect_all_stdout(void)
+{
+    cr_redirect_stdout();
+    cr_redirect_stderr();
+}
+
+Test(test_%%PROJ%%, %%PROJ%%_1)
+{
+    return;
+}
