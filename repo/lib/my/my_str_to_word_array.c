833d514b31523708fbd4a99638a4b818d82e6e02
AngeD <ange.duhayon@epitech.eu>
2021-05-03 09:09:22 +0200

    fix: wrong header

--- repo/lib/my/my_str_to_word_array.c
+++ repo/lib/my/my_str_to_word_array.c
@@ -7,8 +7,6 @@
 
 #include <stdlib.h>
 
-#define VALID(c) !(c == sep || c == skip)
-
 static int is_sep(char const *str, int i, char sep, char skip)
 {
     if (str[i++] != sep)
@@ -21,7 +19,7 @@ static int my_strlen_sep(char const **str, char sep, char skip)
 {
     int size = 0;
 
-    while (!VALID(**str))
+    while ((**str == sep || **str == skip))
         (*str)++;
     for (; **str && **str != sep; (*str)++)
         size += **str != skip;
@@ -32,7 +30,7 @@ static int count_words(char const **str, char sep, char skip)
 {
     int size = 1;
 
-    while (!VALID(**str))
+    while ((**str == sep || **str == skip))
         (*str)++;
     for (int i = 0; (*str)[i]; i++)
         size += is_sep(*str, i, sep, skip);
@@ -72,7 +70,7 @@ char **my_str_to_word_array(char const *str, char sep, char skip)
     if (!arr)
         return NULL;
     for (int i = 0; *str; str++)
-        if (VALID(*str))
+        if (*str != sep && *str != skip)
             arr[i][j++] = *str;
         else if (is_sep(str, 0, sep, skip)) {
             i++;
