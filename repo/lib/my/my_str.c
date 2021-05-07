1bcf1698841dc5e28e4be01be55d370430f9cc1f
AngeD <ange.duhayon@epitech.eu>
2021-05-07 18:13:23 +0200

    fix: my_strcmp

--- repo/lib/my/my_str.c
+++ repo/lib/my/my_str.c
@@ -30,7 +30,10 @@ int my_strcmp(char const *s1, char const *s2)
 {
     if (!(s1 && s2))
         return -1;
-    while (!(*s1++ - *s2++) && *s1 && *s2);
+    while (!(*s1 - *s2) && *s1 && *s2) {
+        s1++;
+        s2++;
+    }
     return *s1 - *s2;
 }
 
