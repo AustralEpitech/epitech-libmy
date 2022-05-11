2ade19a041cf13ad955acb7d22a44d67ae4f8742
AngeD <ange.duhayon@epitech.eu>
2022-05-11 14:10:57 +0200

    feat: branchless ctype conversions

--- repo/include/my_ctype.h
+++ repo/include/my_ctype.h
@@ -15,9 +15,9 @@
     #define ISPRINT(c)  (032 <= (c) && (c) <= 127)
     #define ISALNUM(c)  (ISALPHA(c) || ISDIGIT(c))
 
-    #define TOUPPER(c)  (ISLOWER(c) ? (c) - 'a' + 'A' : (c))
-    #define TOLOWER(c)  (ISUPPER(c) ? (c) - 'A' + 'a' : (c))
-    #define TODIGIT(c)  (ISDIGIT(c) ? (c) - '0' : (c))
+    #define TOUPPER(c)  ((c) - ('a' + 'A') * ISLOWER(c))
+    #define TOLOWER(c)  ((c) - ('A' + 'a') * ISUPPER(c))
+    #define TODIGIT(c)  ((c) - ('0'      ) * ISDIGIT(c))
 
     #include <ctype.h>
 
